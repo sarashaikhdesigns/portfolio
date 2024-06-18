0fb26ba955db443589fe610690dbd86893a915d8
sarashaikhh <sara.shaikh.nc@gmail.com>
2024-06-18 16:51:14 -0400

    interaction

--- interactive/script.js
+++ interactive/script.js
@@ -6,5 +6,19 @@ document.addEventListener("DOMContentLoaded", function () {
       menu.style.display = menu.style.display === 'none' || menu.style.display === '' ? 'flex' : 'none';
   });
 
+  const content = `ARTISTS NOTE: This website is not only my portfolio, but is also one of my biggest personal projects that I coded entirely from scratch. Everything you are navigating through is made with HTML, CSS, and Javascript.`;
+
+      const targetElement = document.getElementById('intro');
+      let index = 0;
+
+      function typeWriter() {
+          if (index < content.length) {
+              targetElement.innerHTML += content.charAt(index);
+              index++;
+              setTimeout(typeWriter, 50); // Adjust the speed by changing the timeout value
+          }
+      }
+
+      typeWriter();
 
 });
